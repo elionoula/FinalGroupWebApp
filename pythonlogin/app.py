8ad7b1bc953f59949c946efb5078ab11bd8c2f01
Elio <elionoula@gmail.com>
2021-05-07 15:18:50 -0400

    EN - Feature: Added login (GET/POST) to app.py

--- pythonlogin/app.py
+++ pythonlogin/app.py
@@ -16,6 +16,24 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'pythonlogin'
 mysql.init_app(app)
 
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return redirect(url_for('home'))
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('index.html', msg=msg)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
