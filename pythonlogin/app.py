8946c12eb04d16dbe13298789db4b871c4b0f613
Elio <elionoula@gmail.com>
2021-05-07 15:20:25 -0400

    EN - Feature: Added register (GET/POST) to app.py

--- pythonlogin/app.py
+++ pythonlogin/app.py
@@ -35,5 +35,34 @@ def login():
             msg = 'Incorrect username/password!'
     return render_template('index.html', msg=msg)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        fullname = request.form['fullname']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s)', (fullname, username, password, email))
+            conn.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
